0614c612dc2768f906e82c4520b09530c1a87b30
Karishma K <karishmak816170@gmail.com>
2026-02-27 23:41:49 +0530

    Add files via upload

--- Loginsinfo.js
+++ Loginsinfo.js
@@ -1,11 +1,27 @@
 import React  from 'react'
-import {useForm} from 'react-hook-form'
+import {useForm} from 'react-hook-form';
 
 function Loginsinfo() {
     const {register, handleSubmit ,formState:{errors}} = useForm(); 
+    const onSubmit = (data) =>{
+      console.log(data);
+    }
   return (
     <>
-    <h1>Login Form</h1>
+    <form style={{marginLeft: '100px',marginTop:'100px'}} onSubmit={handleSubmit(onSubmit)}>
+      <label>Enter Email-ID:</label>
+      <input type="email" 
+      style={{marginLeft:'10px'}}
+      {...register('email',{reuired:true,pattern: /^\S+@\S+$/i})}/>
+      {errors.email && <p>Email is required and must be a vaild email.</p>}
+      <br/>  
+       <label>Enter password:</label>
+      <input type="password"
+      style={{marginLeft:'10px'}}></input>   
+      <br/>
+      <button type="submit" 
+      style={{marginLeft:'150px'}}>Submit</button>   
+      </form>
     </>
   )
 }
