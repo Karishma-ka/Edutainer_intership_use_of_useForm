7184a495b071aa46597a8532d41b8d33b6607d3a
Karishma K <karishmak816170@gmail.com>
2026-03-03 17:05:39 +0530

    Add files via upload

--- Loginsinfo.js
+++ Loginsinfo.js
@@ -1,13 +1,1 @@
-import React  from 'react'
-import {useForm} from 'react-hook-form'
-
-function Loginsinfo() {
-    const {register, handleSubmit ,formState:{errors}} = useForm(); 
-  return (
-    <>
-    <h1>Login Form</h1>
-    </>
-  )
-}
-
-export default Loginsinfo
+h1 className='display-5'
